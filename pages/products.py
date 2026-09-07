af58c5fce478dc7d57fc571a944400041424ee7d
feargodemma234 <feargodemma56@gmail.com>
2026-09-07 14:57:39 +0100

    Delete products.py

--- pages/products.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="Products - Change2")
-st.title("🛍️ Products")
-st.write("Welcome to the shop!")
-st.image("https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400")
-st.button("Back to Home")
