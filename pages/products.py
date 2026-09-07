a624fa195874a8801f96d16d01db2a98194ef9fc
feargodemma234 <feargodemma56@gmail.com>
2026-09-07 14:32:20 +0100

    Create products.py

---
+++ pages/products.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.set_page_config(page_title="Products - Change2")
+st.title("🛍️ Products")
+st.write("Welcome to the shop!")
+st.image("https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400")
+st.button("Back to Home")
